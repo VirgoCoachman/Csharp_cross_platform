14ae0f43b5465a39ef1c547ee60cdea86ca129fd
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-04-02 20:59:56 +0200

    Handler differences in operating systems using OperatingSystem class

--- chapter02-vscode/Arguments/Program.cs
+++ chapter02-vscode/Arguments/Program.cs
@@ -6,3 +6,40 @@ foreach (string arg in args)
 {
     WriteLine(arg);
 }
+
+if (args.Length < 3)
+{
+    WriteLine("You must specify txo colors and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return ; // stop running
+}
+
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0],
+    ignoreCase: true
+);
+
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1],
+    ignoreCase: true
+);
+
+// Setting the cursorSize is only supported on Windows
+// CursorSize = int.Parse(args[2]);
+try
+{
+    CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+    WriteLine("The current platform does not support changing the size of the cursor.");
+}
+
+if (OperatingSystem.IsWindowsVersionAtLeast(major: 10))
+{
+    CursorSize = int.Parse(args[2]);
+}
+
+WriteLine("Hello world!");
