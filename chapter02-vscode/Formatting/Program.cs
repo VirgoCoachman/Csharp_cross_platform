b9cb63b288d1f2acad4db82ad34d2555cd94a632
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-04-02 19:19:54 +0200

    Import System.Console static class using the 'using' directive to avoid repeating every time we want to write something in the console. We can just call Write or WriteLine methods instead directly

--- chapter02-vscode/Formatting/Program.cs
+++ chapter02-vscode/Formatting/Program.cs
@@ -1,8 +1,9 @@
-﻿
+﻿using static System.Console;
+
 int numberOfApples = 12;
 decimal pricePerApple = 0.35M;
 
-Console.WriteLine(
+WriteLine(
     format: "{0} apples costs {1:C}",
     arg0: numberOfApples,
     arg1: pricePerApple * numberOfApples
@@ -14,15 +15,15 @@ string formatted = string.Format(
     arg1: pricePerApple * numberOfApples
 );
 
-Console.WriteLine(formatted);
+WriteLine(formatted);
 
-Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:c}");
+WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:c}");
 
 const string firstname = "Virgo";
 const string lastname = "Coachman";
 const string fullname = $"{firstname} {lastname}";
 
-Console.WriteLine($"My name is : {fullname}");
+WriteLine($"My name is : {fullname}");
 
 string applesText = "Apples";
 int applesCount = 1234;
@@ -30,8 +31,8 @@ int applesCount = 1234;
 string bananasText = "Bananas";
 int bananasCount = 56789;
 
-Console.WriteLine("{0, -10} {1, 6 :N0}", "Name", "Count");
-Console.WriteLine("{0, -10} {1, 6 :N0}", applesText, applesCount);
-Console.WriteLine("{0, -10} {1, 6 :N0}", bananasText, bananasCount);
+WriteLine("{0, -10} {1, 6 :N0}", "Name", "Count");
+WriteLine("{0, -10} {1, 6 :N0}", applesText, applesCount);
+WriteLine("{0, -10} {1, 6 :N0}", bananasText, bananasCount);
 
 
