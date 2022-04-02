6ae11e23b877bad8f83f31d5289403588ea80281
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-04-02 18:53:32 +0200

    Use formatting directly in Console.WriteLine() method and by string.Format() static method

---
+++ chapter02-vscode/Formatting/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+
+Console.WriteLine(
+    format: "{0} apples costs {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples
+);
+
+string formatted = string.Format(
+    format: "{0} apples costs {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples
+);
+
+Console.WriteLine(formatted);
