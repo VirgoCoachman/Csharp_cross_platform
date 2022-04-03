9013b8ed50049ca6894e640c83c8dc8f2d16909a
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-04-03 20:43:00 +0200

    Illustrate pattern matching with the switch statement, and the 'when' key word to  to perform more specific pattern matching

--- chapitre03-vscode/Exercice02/Program.cs
+++ chapitre03-vscode/Exercice02/Program.cs
@@ -11,3 +11,43 @@ else
 {
     WriteLine("obj is not an int so it can't multiply!");
 }
+
+string path = @"/home/virgocoachman/Documents/Workspace/Training/C#/Csharp_cross_platform/chapitre03-vscode/Exercice02";
+
+Write("Press R for read-only or W for writeable : ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+
+Stream? stream;
+
+if (key.Key == ConsoleKey.R)
+{
+    stream = File.Open(Path.Combine(path, "file.txt"), FileMode.OpenOrCreate, FileAccess.Read);
+}
+else
+{
+    stream = File.Open(Path.Combine(path, "file.txt"), FileMode.OpenOrCreate, FileAccess.Write);
+}
+
+string message;
+
+switch (stream)
+{
+    case FileStream writeableFile when stream.CanWrite:
+        message = "The stream is a file that I can write to.";
+        break;
+    case FileStream readOnlyFile:
+        message = "The stream is a read-only file.";
+        break;
+    case MemoryStream ms:
+        message = "The stream is a memoy address.";
+        break;
+    default:
+        message = "The stream is some other type.";
+        break;
+    case null:
+        message = "The stream is null.";
+    break;
+}
+
+WriteLine(message);
