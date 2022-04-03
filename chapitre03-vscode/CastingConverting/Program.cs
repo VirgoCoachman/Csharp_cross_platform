d320c54d5ab144c919a9c2463832f4e354a19a11
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-04-03 22:01:15 +0200

    Show the difference between casting operator and converting method

---
+++ chapitre03-vscode/CastingConverting/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+using static System.Convert;
+
+double g = 9.8;
+int h = ToInt32(g);
+int i = (int)g;
+WriteLine($"g is {g}, h is {h} and i is {i}");
+
+// allocate array of 128 bytes
+byte[] binaryObject = new byte[128];
+
+// populate array with random bytes
+(new Random()).NextBytes(binaryObject);
+
+WriteLine("Binary object as bytes: ");
+
+for (int index = 0; index < binaryObject.Length; index++)
+{
+    Write($" {binaryObject[index]:X}");
+}
+WriteLine();
+
+// convert to Base64 string and output as text
+string encoded = ToBase64String(binaryObject);
+
+WriteLine($"Binary Object as Base64: {encoded}");
+
+
