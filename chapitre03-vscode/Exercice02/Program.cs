1598fc480e9fadc91273ca5e943d4de0f32ee116
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-04-03 20:21:55 +0200

    Illustrate pattern matching with the if statement

--- chapitre03-vscode/Exercice02/Program.cs
+++ chapitre03-vscode/Exercice02/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+object obj = 3;
+int j = 4;
+
+if (obj is int i)
+{
+    WriteLine($"{i} x {j} = {i * j}");
+}
+else
+{
+    WriteLine("obj is not an int so it can't multiply!");
+}
