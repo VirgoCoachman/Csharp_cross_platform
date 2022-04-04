1221e1bd60c02c1be0727ef00ec79fe4fcf6d2a6
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-04-04 04:18:05 +0200

    Handle exception using a specific type of (specialized) Exception above the general class Exception

--- chapitre03-vscode/HandlingExceptions/Program.cs
+++ chapitre03-vscode/HandlingExceptions/Program.cs
@@ -9,6 +9,10 @@ try
     int age = int.Parse(input);
     WriteLine($"You are {age} years old.");
 }
+catch (FormatException ex)
+{
+    WriteLine("The age you entered is not a valid number format.");
+}
 catch (Exception ex)
 {
     WriteLine($"{ex.GetType()} says {ex.Message}");
