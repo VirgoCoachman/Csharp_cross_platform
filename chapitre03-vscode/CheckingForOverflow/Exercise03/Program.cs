e415cd3ddad62bcdd6e985eee0ed4b3f8b32e0f6
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-04-04 05:02:17 +0200

    Replace any number divisible by 'three' with the word 'Fizz', any number divisible by 'five' with the word 'Buzz', and any number divisible by both with 'FizzBuzz'

--- chapitre03-vscode/CheckingForOverflow/Exercise03/Program.cs
+++ chapitre03-vscode/CheckingForOverflow/Exercise03/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0)
+    {
+        Write("Fizz");
+    }
+    if (i % 5 == 0)
+    {
+        Write("Buzz");
+    }
+    if (i % 3 != 0 && i % 5 != 0)
+    {
+        Write(i);
+    }
+    if (i < 100)
+    {
+        Write(", ");
+    }
+    if (i % 10 == 0)
+    {
+        WriteLine();
+    }
+}
