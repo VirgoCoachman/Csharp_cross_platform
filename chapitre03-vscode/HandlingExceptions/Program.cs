91e80b78df9c761e42e6f3b4ef3bb3773ff956ed
VirgoCoachman <ben.kanku.barthelemy@gmail.com>
2022-04-04 04:10:26 +0200

    Handle exception with an empty catch statement

---
+++ chapitre03-vscode/HandlingExceptions/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+WriteLine("Before parsing");
+Write("What is your age? ");
+string? input = ReadLine();
+
+try
+{
+    int age = int.Parse(input);
+    WriteLine($"You are {age} years old.");
+}
+catch
+{
+    
+}
+WriteLine("After parsing");
